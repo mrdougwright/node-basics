7b41b25bfac9287d93413a50bdee35d5bc4205bd
Doug Wright <mrdougwright@gmail.com>
2020-08-19 16:55:31 -0500

    lesson 7

--- app.js
+++ app.js
@@ -9,12 +9,16 @@ app.set("view engine", "ejs")
 app.listen(3030)
 
 app.get("/", (req, res) => {
-  // res.send("<p>home page</p>")
-  res.sendFile("./views/index.html", { root: __dirname })
+  const blogs = [
+    { title: "Yoshi", snippet: "Some text" },
+    { title: "Mario", snippet: "Some more text" },
+    { title: "Bowser", snippet: "Some more more text" },
+  ]
+  res.render("index", { title: "Index", blogs })
 })
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname })
+  res.render("about", { title: "About" })
 })
 
 // redirects
@@ -22,8 +26,12 @@ app.get("/about-us", (req, res) => {
   res.redirect("/about")
 })
 
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a blog" })
+})
+
 // 404
 // app.use fired for every request, IF it gets here after not matching above
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname })
+  res.status(404).render("404", { title: "404" })
 })
